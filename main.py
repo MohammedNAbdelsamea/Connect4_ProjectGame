2f9afbd06b630df11fa413754d7aebc8bb5796ba
MoNaSser8 <11410120200479@stud.cu.edu.eg>
2023-05-19 01:27:35 +0300

    finishing the project with creating GUI and Draw the graph

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import numpy as np
 import pygame
+import sys
 import math
 import random
 import tkinter as tk
 from tkinter import messagebox
 import matplotlib.pyplot as plt
+import time
 
 BLUE = (255, 255, 0)
 BLACK = (0,0,0)
@@ -246,3 +248,167 @@ def minimax_ab(board, depth, alpha, beta, maximizing_player):
             if alpha >= beta:
                 break
         return column, value
+
+board = create_board()
+print_board(board)
+game_over = False
+turn = 0
+
+# Calling function draw_board again
+draw_board(board)
+pygame.display.update()
+
+myfont = pygame.font.SysFont("Montserrat", 75)
+
+#start_time = time.time()
+
+def run_game(algorithm, difficulty):
+    # Your game logic here
+
+    board = create_board()
+
+    # Game loop
+    game_over = False
+    turn = random.choice([1, 2])
+    start_time = time.time()
+    while not game_over:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        # Player 1 AI's turn
+        if turn == PLAYER:
+            if algorithm=="Minimax":
+                move_start_time = time.time()
+                #minimax_scores_g.append(elapsed_time)
+                if difficulty=="Easy":
+                    col, minimax_score = minimax(board, 1, True)
+                elif difficulty == "Medium":
+                    col, minimax_score = minimax(board, 3, True)
+                elif difficulty == "Hard":
+                    col, minimax_score = minimax(board, 5, True)
+
+                elapsed_time = time.time() - move_start_time
+
+                # Store the elapsed time in the appropriate list
+                minimax_scores_g.append(elapsed_time)
+
+            elif algorithm=="Minimax with Alpha-Beta Pruning":
+                move_start_time = time.time()
+                #minimax_ab_scores_g.append(elapsed_time)
+                if difficulty=="Easy":
+                    col, minimax_ab_score = minimax_ab(board, 1, -math.inf, math.inf, True)
+                elif difficulty == "Medium":
+                    col, minimax_ab_score = minimax_ab(board, 3, -math.inf, math.inf, True)
+                elif difficulty == "Hard":
+                    col, minimax_ab_score = minimax_ab(board, 5, -math.inf, math.inf, True)
+
+                elapsed_time = time.time() - move_start_time
+
+                # Store the elapsed time in the appropriate list
+                minimax_ab_scores_g.append(elapsed_time)
+
+        # Player 2 AI's turn
+        else:
+            if algorithm=="Minimax":
+                move_start_time = time.time()
+                #minimax_scores_g.append(elapsed_time)
+                if difficulty=="Easy":
+                    col, minimax_score = minimax(board, 1, False)
+                elif difficulty == "Medium":
+                    col, minimax_score = minimax(board, 3, False)
+                elif difficulty == "Hard":
+                    col, minimax_score = minimax(board, 5, False)
+
+            elif algorithm=="Minimax with Alpha-Beta Pruning":
+                move_start_time = time.time()
+                #minimax_ab_scores_g.append(elapsed_time)
+                if difficulty=="Easy":
+                    col, minimax_ab_score = minimax_ab(board, 1, -math.inf, math.inf, False)
+                elif difficulty == "Medium":
+                    col, minimax_ab_score = minimax_ab(board, 3, -math.inf, math.inf, False)
+                elif difficulty == "Hard":
+                    col, minimax_ab_score = minimax_ab(board, 5, -math.inf, math.inf, False)
+
+                elapsed_time = time.time() - move_start_time
+
+                # Store the elapsed time in the appropriate list
+                minimax_ab_scores_g.append(elapsed_time)
+
+        if is_valid_location(board, col):
+            pygame.time.wait(500)
+            row = get_next_open_row(board, col)
+            drop_piece(board, row, col, turn + 1)
+
+            if winning_move(board, turn + 1):
+                label = myfont.render(f"AI {turn + 1} wins!", 1, YELLOW if turn == AI else RED)
+                screen.blit(label, (40, 10))
+                game_over = True
+            elif is_terminal_node(board):  # Check for a draw
+                label = myfont.render("Draw!", 1, YELLOW if turn == AI else RED)
+                screen.blit(label, (150, 10))
+                game_over = True
+
+            print_board(board)
+            draw_board(board)
+
+            turn += 1
+            turn %= 2
+
+        if game_over:
+            pygame.time.wait(3000)
+
+        # messagebox.showinfo("Game Result", f"Algorithm: {algorithm}\nDifficulty: {difficulty}")
+    total_elapsed_time = time.time() - start_time
+    print(f"Total elapsed time for the game: {total_elapsed_time} seconds")
+#elapsed_time = time.time() - start_time
+
+def start_game():
+    algorithm = algorithm_var.get()
+    difficulty = difficulty_var.get()
+
+    run_game(algorithm, difficulty)
+
+# Create the main window
+window = tk.Tk()
+window.title("Game Options")
+
+# Algorithm selection
+algorithm_label = tk.Label(window, text="Select Algorithm:")
+algorithm_label.pack()
+
+algorithm_var = tk.StringVar(value="Minimax")
+algorithm_option1 = tk.Radiobutton(window, text="Minimax", variable=algorithm_var, value="Minimax")
+algorithm_option2 = tk.Radiobutton(window, text="Minimax with Alpha-Beta Pruning", variable=algorithm_var, value="Minimax with Alpha-Beta Pruning")
+algorithm_option1.pack()
+algorithm_option2.pack()
+
+# Difficulty selection
+difficulty_label = tk.Label(window, text="Select Difficulty:")
+difficulty_label.pack()
+
+difficulty_var = tk.StringVar(value="Easy")
+difficulty_option1 = tk.Radiobutton(window, text="Easy", variable=difficulty_var, value="Easy")
+difficulty_option2 = tk.Radiobutton(window, text="Medium", variable=difficulty_var, value="Medium")
+difficulty_option3 = tk.Radiobutton(window, text="Hard", variable=difficulty_var, value="Hard")
+difficulty_option1.pack()
+difficulty_option2.pack()
+difficulty_option3.pack()
+
+# Start button
+start_button = tk.Button(window, text="Start Game", command=start_game)
+start_button.pack()
+
+# Run the main event loop
+window.mainloop()
+
+window.mainloop()
+
+# Plotting the graph
+plt.plot(range(len(minimax_scores_g)), minimax_scores_g, label='Minimax',marker='o',markerfacecolor='green')
+plt.plot(range(len(minimax_ab_scores_g)), minimax_ab_scores_g, label='Minimax with Alpha-Beta Pruning',marker='o',markerfacecolor='green')
+plt.xlabel('Game Iteration')
+plt.ylabel('Elapsed Time (seconds)')
+plt.title('Algorithm Performance Comparison')
+plt.legend()
+plt.show()
